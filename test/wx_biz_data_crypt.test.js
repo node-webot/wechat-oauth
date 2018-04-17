8b2dc54b231d4ea170d3b37e5f8ed4f84fb1bf55
aojiaotage <aojiaotage@gmail.com>
2018-04-17 16:27:33 +0800

    Unit test on wechat decryption class.

---
+++ test/wx_biz_data_crypt.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var crypto = require('crypto');
+var expect = require('expect.js');
+var urllib = require('urllib');
+var WxBizCrypt = require('../lib/wx_biz_data_crypt');
+
+describe('wx_biz_data_crypt.js', function () {
+  describe('decryptData', function () {
+    var appId = 'appId';
+    var sessionKey = crypto.randomBytes(16).toString('base64');
+    var iv = crypto.randomBytes(16).toString('base64');
+    var data = {
+      openid: 'openid',
+      exampleField1: 'just a example',
+      watermark: {
+        appid: appId,
+      },
+    };
+
+    var cipher = crypto.createCipheriv('aes-128-cbc', new Buffer(sessionKey, 'base64'), new Buffer(iv, 'base64'));
+    cipher.setAutoPadding(true);
+    var encryptedData = cipher.update(JSON.stringify(data), 'utf8', 'base64');
+    encryptedData += cipher.final('base64');
+
+    var cryptor = new WxBizCrypt(appId, sessionKey);
+
+    it('should ok', function () {
+      var decryptedData = cryptor.decryptData(encryptedData, iv);
+      expect(decryptedData.openid).to.equal(data.openid);
+      expect(decryptedData.exampleField1).to.equal(data.exampleField1);
+      expect(decryptedData.watermark.appid).to.equal(data.watermark.appid);
+    });
+
+    it('should not ok on invalid decrypted data input', function (){
+      expect(cryptor.decryptData).withArgs('', iv).to.throwError();
+    });
+
+    it('should not ok on invalid decrypted data input', function (){
+      expect(cryptor.decryptData).withArgs(encryptedData, '').to.throwError();
+    });
+
+    it('should not ok on invalid app id', function (){
+      var invalidCryptor = new WxBizCrypt('invalid app id', sessionKey);
+      expect(invalidCryptor.decryptData).withArgs(encryptedData, '').to.throwError();
+    });
+
+  });
+});
